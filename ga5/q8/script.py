1674aee1651924d087505b534a103d145de7c321
23f3003994 <23f3003994@ds.study.iitm.ac.in>
2026-03-10 22:56:41 +0530

    ga5: q8 done

---
+++ ga5/q8/script.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv("q-stock-prices-ema.csv")
+
+df["EMA_21"] = df.groupby("Ticker")["Close_Price"].transform(
+    lambda x: x.ewm(span=21, adjust=False).mean()
+)
+print(df.head())
+last_date = df["Date"].max()
+
+last_day = df[df["Date"] == last_date]
+
+row = last_day.loc[last_day["EMA_21"].idxmax()]
+
+ema_value = round(row["EMA_21"], 2)
+ticker = row["Ticker"]
+
+print(f"{ema_value}, {ticker}")
+
